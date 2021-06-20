f27b660fc00883b1ce9f04c9f03b836e0baa8d04
dhartmann1979 <66730370+dhartmann1979@users.noreply.github.com>
2021-06-20 18:50:07 +0200

    Update iot-lora-gateway-start.sh

--- iot-lora-gateway-start.sh
+++ iot-lora-gateway-start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 python /opt/iotloragateway/configureGateway.py
-if [ ! -d "/sys/class/gpio/gpio22" ]; then
-    echo "22" > /sys/class/gpio/export
+if [ ! -d "/sys/class/gpio/gpio25" ]; then
+    echo "25" > /sys/class/gpio/export
 fi
 
-echo "out" > /sys/class/gpio/gpio22/direction
-echo "1" > /sys/class/gpio/gpio22/value
+echo "out" > /sys/class/gpio/gpio25/direction
+echo "1" > /sys/class/gpio/gpio25/value
 sleep 1
-echo "0" > /sys/class/gpio/gpio22/value
+echo "0" > /sys/class/gpio/gpio25/value
 
 /opt/iotloragateway/iot-lora-gateway
